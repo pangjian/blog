84ca8ce8fd9c979d3f99d38ff0a1c18bebaea513
pangjian <pangjian1010@gmail.com>
2015-02-27 15:06:27 +0800

    发布访问数统计半成品

--- source/_drafts/realtime-count-firebase.md
+++
@@ -1,6 +0,0 @@
-title: 基于FireBase的Hexo博客实时访问数
-categories: 技术
-tags: [技术,firebase,hexo]
----
-
-本来想写一个HEXO的搭建过程的，但是在搜索引擎上一搜一大把教程。看到有人评论：“你写这个跟HEXO官方的教程比好在哪？”仔细想想，说的极好。索性也就不写这个人家已经写烂了的东西了。付一个[官方教程链接](http://hexo.io/docs/)。其实在搭建过程中还参考了一个很全面很详细的博客，也附上链接吧，[ibruce不如](http://ibruce.info/2013/11/22/hexo-your-blog/)。
--- themes/landscape-plus/source/js/count.js
+++ themes/landscape-plus/source/js/count.js
@@ -1,5 +1,7 @@
-// 总数
+
 var io10blogFirebase = new Firebase("https://io10.firebaseio.com/");
+
+// 获取总数，并将总访问量展示在页面上
 io10blogFirebase.child("sum").on("value", function(data) {
   var current_counter = data.val();
   if( $("#counter").length > 0  && current_counter >1 ){
@@ -9,17 +11,21 @@ io10blogFirebase.child("sum").on("value", function(data) {
   };
 });
 
+// 总数+1
 io10blogFirebase.child("sum").transaction(function (current_counter) {
   return (current_counter || 0) + 1;
 });
 
-// 明细
+// 明细由当前页面的url表示，将反斜线替换成下划线，并将中文decode出来
 var current_url = decodeURI(window.location.pathname.replace(new RegExp('\\/|\\.', 'g'),"_"));
 
+// 明细+1
 io10blogFirebase.child("detail/"+current_url).transaction(function (current_counter) {
   return (current_counter || 0) + 1;
 });
 
+
+// 记录最后更新时间
 var n = new Date();
 var time = n.getFullYear()+'-'+(n.getMonth()+1)+'-'+n.getDate()+'_'+n.getHours()+':'+n.getMinutes()+':'+n.getSeconds()+' '+n.getMilliseconds();
 io10blogFirebase.child("lastupdatetime").set({ timer: time, url: current_url });
