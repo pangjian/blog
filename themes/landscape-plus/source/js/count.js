7c8b678ab1304fd69fb233f27cea287b9a6849a0
pangjian <pangjian1010@gmail.com>
2015-02-28 16:09:27 +0800

    增加当前页面访问数

--- themes/landscape-plus/source/js/count.js
+++ themes/landscape-plus/source/js/count.js
@@ -1,14 +1,27 @@
 
 var io10blogFirebase = new Firebase("https://io10.firebaseio.com/");
 
+// 明细由当前页面的url表示，将反斜线替换成下划线，并将中文decode出来
+var current_url = decodeURI(window.location.pathname.replace(new RegExp('\\/|\\.', 'g'),"_"));
+
 // 获取总数，并将总访问量展示在页面上
 io10blogFirebase.child("sum").on("value", function(data) {
   var current_counter = data.val();
-  if( $("#counter").length > 0  && current_counter >1 ){
-      $("#counter").html(
-   	   	"&nbsp;|&nbsp;本站总访问量&nbsp;<font style='color:white'>"+ current_counter +"</font>&nbsp;次"
+  if( $("#sum_counter").length > 0  && current_counter >1 ){
+      $("#sum_counter").html(
+   	   	"&nbsp;|&nbsp;总访问量&nbsp;<font style='color:white'>"+ current_counter +"</font>&nbsp;次"
        );
   };
+});
+
+// 获取明细，并将明细也展示在页面上
+io10blogFirebase.child("detail/"+current_url).on("value", function(data){
+	var detail_counter = data.val();
+	if($("#detail_counter").length > 0 && detail_counter > 1){
+		$("#detail_counter").html(
+			"&nbsp;本页访问量&nbsp;<font style='color:white'>"+ detail_counter +"</font>&nbsp;次"
+		);
+	}
 });
 
 // 总数+1
@@ -16,8 +29,6 @@ io10blogFirebase.child("sum").transaction(function (current_counter) {
   return (current_counter || 0) + 1;
 });
 
-// 明细由当前页面的url表示，将反斜线替换成下划线，并将中文decode出来
-var current_url = decodeURI(window.location.pathname.replace(new RegExp('\\/|\\.', 'g'),"_"));
 
 // 明细+1
 io10blogFirebase.child("detail/"+current_url).transaction(function (current_counter) {
